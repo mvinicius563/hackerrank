ed40ab5e89f85ae62c34aa21891b56b33548f832
Marcus Custodio <mvinicius.custodio@gmail.com>
2016-05-29 10:41:01 +0200

    create folder for 30DaysOfCOde

--- Hackerrank_cs/Hackerrank_cs/Program.cs
+++ Hackerrank_cs/Hackerrank_cs/Program.cs
@@ -1,11 +1,5 @@
 ﻿namespace Hackerrank_cs
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
     class Program
     {
         static void Main(string[] args)
