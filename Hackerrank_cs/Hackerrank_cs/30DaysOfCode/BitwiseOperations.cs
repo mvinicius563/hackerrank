5d0d6ab31bab51a1895154eea98b157f7ed764fb
Marcus Custodio <mvinicius.custodio@gmail.com>
2016-06-04 23:12:29 +0200

    BitwiseOperations

---
+++ Hackerrank_cs/Hackerrank_cs/30DaysOfCode/BitwiseOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerrank_cs._30DaysOfCode
+{
+    class BitwiseOperations
+    {
+        static int get(int n, int k)
+        {
+            if ((k | (k - 1)) > n)
+            {
+                return k - 2;
+            }
+            else
+            {
+                return k - 1;
+            }
+        }
+
+        public BitwiseOperations()
+        {
+            int n = 8;
+            int k = 5;
+            Console.WriteLine(5 | 4);
+
+            Console.WriteLine("=====" + get(n, k));
+            
+        }
+    }
+}
